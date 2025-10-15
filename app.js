09b1149e207394720cc1552afa4b4e3720968a75
eedo_y <doup2001@naver.com>
2025-10-15 16:13:59 +0900

    폴더 이동

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const port = 8080;
 // 현재 디렉터리의 모든 파일을 정적(static)으로 제공
 app.use(express.static('public'));
 
-// 루트("/") 접근 시 html/index.html로 리다이렉트
+// 루트("/") 접근 시 pages/index.html로 리다이렉트
 app.get('/', (req, res) => {
-    res.redirect('/html/index.html');
+    res.redirect('/pages/html/index.html');
 });
 
 // 서버 실행
