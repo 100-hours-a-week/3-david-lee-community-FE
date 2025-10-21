950c04b91e03eb571d48bf914bc0f72dab315cf7
eedo_y <doup2001@naver.com>
2025-10-21 19:15:46 +0900

    ✨feat : appFooter 추가

---
+++ public/pages/common/appfooter.js
@@ -0,0 +1,9 @@
+/// 푸터 로드
+const footer = document.querySelector('.appfooter');
+
+/// 내부 HTML 주입
+footer.innerHTML = `
+<div class="footer__inner">
+  <p>© 2025 아무 말 대잔치. All rights reserved.</p>
+</div>
+`;
