f359c5eabf34e31c0baf27c84c55a80c987fb3e5
eedo_y <doup2001@naver.com>
2025-10-25 17:24:10 +0900

    ✨feat : 글 작성자와 댓글 작성자 여부 판단

--- public/components/commentCard.js
+++ public/components/commentCard.js
@@ -54,6 +54,13 @@ export async function createCommentCard(comment, {onCommentReply, onCommentEdit,
     const delBtn = $toolbar.querySelector('[data-action="delete"]');
     const replyBtn = node.querySelector('[data-action="reply"]');
 
+    console.log(comment);
+
+    /// 게시글 작성자와 동일하다면, 색깔을 파랗게 처리
+    if (comment.writer === true) {
+        $authorName.insertAdjacentHTML('beforeend', ' <span class="writer-badge">작성자</span>');
+    }
+
     if (comment.editable) {
         /// 수정 버튼 클릭
         editBtn?.addEventListener('click', (e) => {
