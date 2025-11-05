a6ff514c9a361e003ad68c4e0b26202c4bcc9927
eedo_y <doup2001@naver.com>
2025-11-05 17:14:15 +0900

    🔥 fix: 약관 페이지는 푸터에서 사용되던 문제 수정

--- public/api/config.js
+++ public/api/config.js
@@ -3,7 +3,6 @@ export const API_BASE = `${location.protocol}//${location.hostname}:8080/v1`;
 
 export const AUTH_URL = `${API_BASE}/auth`;
 export const USER_URL = `${API_BASE}/users`;
-export const POLICY_URL = `http://3.38.66.186:8080/v1/policy`;
 export const POST_URL = `${API_BASE}/posts`;
 export const LIKE_URL = `${API_BASE}/posts/likes`;
 export const IMAGE_URL = `${API_BASE}/images`;
--- public/pages/common/appfooter.js
+++ public/pages/common/appfooter.js
@@ -1,12 +1,14 @@
 /// 푸터 로드
+import {API_BASE} from "../../api/config.js";
+
 const footer = document.querySelector('.appfooter');
 
 /// 내부 HTML 주입
 footer.innerHTML = `
   <div class="footer__inner">
-    <a href="http://localhost:8080/v1/policy/terms">이용약관</a>
+    <a href="${API_BASE}/policy/terms">이용약관</a>
     <span class="divider">|</span>
-    <a href="http://localhost:8080/v1/policy/privacy">개인정보처리방침</a>
+    <a href="${API_BASE}/policy/privacy">개인정보처리방침</a>
     <span class="divider">|</span>
     <p class="footer__copy">© 2025 아무 말 대잔치. All rights reserved.</p>
   </div>
